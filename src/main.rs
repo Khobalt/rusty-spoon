f27650b9688729e6a31304277f15716c5374afa8
Lee Dydo <lee@dydo.co>
2021-02-15 12:29:22 -0500

    Spawn eight threads and start printing, willy-nilly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![forbid(unused_imports)]
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    play_with_threads();
+}
+
+
+fn play_with_threads(){
+    let mut joinhandles = Vec::new();
+    
+    for offset in 0..8 {
+        joinhandles.push(thread::spawn(move || {
+            for i in 0..10000 {
+                println!("I'm thread {}, spitting out the number {}", offset, i);
+            }
+        }));
+    }
+    for handle in joinhandles.into_iter() {
+        handle.join().unwrap();
+    }
+
 }
